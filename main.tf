3b0f8dedc5037c82b850b591e68e499c3d07362d
HayBam <ayobami.odunlami@hotmail.com>
2024-11-06 17:59:27 -0500

    configure security group

--- main.tf
+++ main.tf
@@ -15,3 +15,14 @@ resource "aws_instance" "example" {
 		Name = "TerraformExample"
 	}
 }
+
+resource "aws_security_group" "instance" {
+	name = "terraform-example-instance"
+
+	ingress {
+		from_port = 8080
+		to_port = 8080
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+		}
+}
