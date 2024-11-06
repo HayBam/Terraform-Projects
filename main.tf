ae3b9501a984b7c54497a92d03be17e81d4ca945
HayBam <ayobami.odunlami@hotmail.com>
2024-11-06 17:53:16 -0500

    bootstrap config busybox

--- main.tf
+++ main.tf
@@ -4,6 +4,12 @@ provider "aws" {
 resource "aws_instance" "example" {
 	ami = "ami-06b21ccaeff8cd686"
 	instance_type = "t2.micro"
+
+	user_data = <<-EOF
+			#!/bin/bash
+			echo "Hello Welcome to Terraform World" > index.html
+			nohup busybox httpd -f -p 8080 &
+			EOF
 	
 	tags = {
 		Name = "TerraformExample"
