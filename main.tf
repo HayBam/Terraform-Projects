c44d04f567f69453aacafafbb542818e0047ffa7
HayBam <ayobami.odunlami@hotmail.com>
2024-11-06 18:08:50 -0500

    apply vpc id to instance

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ provider "aws" {
 resource "aws_instance" "example" {
 	ami = "ami-06b21ccaeff8cd686"
 	instance_type = "t2.micro"
+	vpc_security_group_ids = [aws_security_group.instance.id]
 
 	user_data = <<-EOF
 			#!/bin/bash
